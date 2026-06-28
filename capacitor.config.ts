13aacf52426349f4a57c2de80e9ac9df8b3442d5
Charesmagna <tshilidzi.mukwevho54@gmail.com>
2026-06-28 04:24:38 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.tmml.safetylink',
-  appName: 'SafetyLink Core',
+  appId: 'com.safetylink.app',
+  appName: 'SafetyLink',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
